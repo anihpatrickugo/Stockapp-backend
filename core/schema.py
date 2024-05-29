2284248c2029c0b1f90b5f8df15ad203e824f691
anihpatrickugo <iampatrickugo@gmail.com>
2024-05-30 00:03:53 +0100

    minimum deposit amount and withdrawal

--- core/schema.py
+++ core/schema.py
@@ -5,7 +5,10 @@ from transactions.schema import TransactionsMutations, TransactionsQuery
 from portfolio.schema import PortfolioQuery, PortfolioMutations
 
 class Query(AuthQuery, TransactionsQuery, PortfolioQuery, graphene.ObjectType):
+    # These are Queries for the Site Details
     wallet_address = graphene.String(default_value=settings.SITE_USDT_WALLET_ADDRESS)
+    minimum_deposit = graphene.Int(default_value=settings.SITE_MINIMUM_DEPOSIT_AMAOUNT)
+    minimum_withdrawal = graphene.Int(default_value=settings.SITE_MINIMUM_WITHDRAWAL_AMAOUNT)
 
 
 class Mutation(AuthMutations, TransactionsMutations, PortfolioMutations, graphene.ObjectType):
